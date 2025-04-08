a2e72d0398a093a9e0fa555281e74ea7d6598015
P3578 <parthpatel089.pp@gmail.com>
2025-04-08 15:08:12 +0530

    Add files via upload

---
+++ assignment 2/q3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#define pie 3.14
+int main()
+{
+int n1=5;
+char a='A';
+float n2=10.5;
+/* here take n1 for the any numerical value.
+	Take n2 for the float value.
+	Take a for any alphabetic value.
+	Take pie is for constant.*/
+printf("\n the value of n1=%d",n1);
+printf("\n the value of n2=%f",n2);
+printf("\n the value of a= %c",a);
+printf("\n the value of pie=%.2f",pie);
+return 0;
+}
---
+++ assignment 2/q8.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+//without return type with argument 
+void fact(int n1);//declaration
+void fact(int n1)//defition
+{
+    int ans = 1,i;
+    for (i=1;i<=n1;i++)
+    {
+        ans = ans*i;
+    }
+    printf ("\n the factorial of n1 = %d",ans);
+}
+int main()
+{
+    int num1;
+    printf ("\n enter the number = ");
+    scanf ("%d",&num1);
+    fact(num1);//calling
+    return 0;
+}
